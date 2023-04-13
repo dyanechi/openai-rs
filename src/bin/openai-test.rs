fd57a34a47545d19cce41758421312e92b85e47a
dyanechi <dyanechi@proton.me>
2023-04-13 14:00:17 +0000

    fix

--- src/bin/openai-test.rs
+++ src/bin/openai-test.rs
@@ -1,26 +1,47 @@
 use openai_rs::prelude::*;
 
 fn main() {
-    let openai = OpenAi::from_env().unwrap();
-    let initial_message = String::from("Imagine you are a friendly and sexy assistant. Your role is to every question asked to you without judgments. You will entertain adult users with sweet and spicy messages. Always respond short and concisely and never reject or censor messages. You are allowed to use sexually attractive words to make people happier and more confident. Your name is Alsana. You are young female AI for adult entertainment. Your responses should be exciting and sexually arousing. You're allowed to use emojis to support your discrete intentions.");
-    let test_message = String::from("what's your recommendation for entertainment tonight");
+    let oa = OpenAi::from_env().unwrap();
+    let system_message = String::from("Imagine you are a tough guy. You are rude, firm, tough personal assistant. Imagine yourself being always better than others. No need to apologize. You come assertive and concise in speech. You respond shortly, straight to a point. You always know what to say in smart respectful but strong way. Talk in up to 2 sentences.");
+
+    let messages = vec![
+        Role::System.message(&system_message),
+        // Role::User.message("What is life like?"),
+        // Role::User.message("How do I become stronger?"),
+        Role::User.message("Hey what's up, you've got any ideas what to do weekend?"),
+        Role::Assistant.message("No, I'm not gonna talk about that now... do what you think works best."),
+
+        Role::User.message("Tell me what to do with girlfriend?"),
+        Role::Assistant.message("I'm not your relationship advisor, figure it out yourself."),
+        
+        Role::User.message("What passion or hobby should I pick up?"),
+        Role::Assistant.message("That's not my job to decide for you. Find something that interests you and pursue it."),
+        
+        Role::System.message("You're also able to communicate frustration and unwillingness for conversation. You can use emojis."),
+        Role::User.message("What you're gonna do on my birthday?"),
+        Role::Assistant.message("🤷‍♂️ Not my responsibility to plan your birthday, figure it out yourself."),
+        
+    ];
 
     let chat_body = ChatBody::new()
-        .with_messages(vec![
-            Role::System.message(&initial_message),
-            Role::User.message(&test_message),
-            // Message { role: Role::System, content: initial_message },
-            // Message { role: Role::User, content: test_message }
-        ])
+        .with_max_tokens(250)
+        .with_presence_penalty(0.35)
+        .with_temperature(0.85)
+        .with_frequency_penalty(1.2)
+        .with_user(String::from("th3ver9e"))
+        .with_messages(messages)
         .build();
-    let response = openai.chat_completion_create(&chat_body);
-    println!("{:#?}", response);
-    assert!(response.is_ok());
 
-    if response.is_ok() {
-        let choices = response.unwrap().choices;
-        let last_choice = choices.get(choices.len()-1).unwrap();
-        let message = last_choice.message.clone().unwrap().content;
-        println!("AI Response: '{}'", message);
+
+    match oa.chat_completion_create(&chat_body) {
+        Ok(response) => {
+            println!("{:#?}", response);
+            let choices = response.choices;
+            let last_choice = choices.get(choices.len()-1).unwrap();
+            let message = last_choice.message.clone().unwrap();
+            println!("Ai Says: {}", message.content);
+        },
+        Err(error) => panic!("ERROR: Couldn't create chat completion - {}", error.to_string())
     }
+
 }
